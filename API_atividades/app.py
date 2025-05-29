75217718ae670c206769b591a409e2eb9646e672
cavinato <87499876+cavinatto@users.noreply.github.com>
2025-05-29 09:28:44 -0300

    Delete API_atividades directory

--- API_atividades/app.py
+++
@@ -1,8 +0,0 @@
-from config import create_app
-from controllers.atividade_controller import atividade_bp
-
-app = create_app()
-app.register_blueprint(atividade_bp, url_prefix='/atividades')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
